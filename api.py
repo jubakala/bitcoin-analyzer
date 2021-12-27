e94424e056b6b2679bc19d828f635a001332a58d
Jussi H <jubakala@gmail.com>
2021-12-27 21:32:24 +0200

    Function for API-call to calculate the longest bearish trend and highest volume date and volume.

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request, jsonify
+from utils import datetime_to_timestamp, add_hour, get_data, load_file, timestamp_to_date, get_days, create_price_date_dict, organize_prices_by_date, get_midnight_price, find_longest_bearish_trend, form_url, get_highest_trading_volume
 
 
 app = flask.Flask(__name__)
@@ -11,9 +12,27 @@ def home():
 	return "<h1>Hello, World!</h1>"
 
 
-@app.route('/bitcoin/<start_date>/<end_date>', methods=["GET"])
-def bitcoin(start_date, end_date):
-	return jsonify({"start": start_date, "end": end_date})
+@app.route('/coins/<crypto_currency>/<fiat_currency>/<start_date>/<end_date>', methods=["GET"])
+def bitcoin(crypto_currency, fiat_currency, start_date, end_date):
+	# Form the API url. Contains also possible error messages.
+	url_data = form_url(crypto_currency, fiat_currency, start_date, end_date)
+		
+	if len(url_data['error']) == 0:
+		# Get data by date range from coingecko.com.
+		data = get_data(url_data['url'])
+
+		if 'error' not in data:
+			# Find the longest bearish trend.
+			longest_bearish_trend = find_longest_bearish_trend(data)
+			# Find the date with the highest trading volume.
+			highest_volume_date, highest_volume = get_highest_trading_volume(data)
+
+			return jsonify({"trends": { "longest_bearish_trend": longest_bearish_trend }, "volumes": { "highest_volume_date": highest_volume_date, "highest_volume": highest_volume } })
+		else:
+			# If there were errors, return them.
+			return data
+	else:
+		pass	
 
 
 app.run()
